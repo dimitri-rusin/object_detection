98f26e89a36d7883be39d25a90c8a20a31d442ea
Dimitri <habimm@pm.me>
2023-05-12 16:35:04 +0200

    put the link of the input image

--- user.py
+++ user.py
@@ -1,6 +1,7 @@
 from info import info
 import requests
 
+# https://ultralytics.com/images/bus.jpg
 with open('bus.jpg', 'rb') as binary_image:
     response = requests.post('http://192.168.178.160:8080/yolo', data=binary_image)
 
