0006611b5218f459ce8546a3b880deb30a163d2a
Dimitri <habimm@pm.me>
2023-05-12 15:50:49 +0200

    don't use "reload for web server development" functionality

--- api.py
+++ api.py
@@ -59,4 +59,4 @@ def add_cors_headers(response):
   return response
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=8080, debug=True)
+  app.run(host='0.0.0.0', port=8080, debug=True, use_reloader=False)
